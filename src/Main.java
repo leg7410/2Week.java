75649efac48250613b82166a873b6ff3bde02148
user <202430325@email.daelim.ac.kr>
2024-09-11 17:19:48 +0900

    d

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
+class Hello2 {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); //안녕 세상아
+        /*
+        안
+        녕
+        세
+        상
+        아
+         */
+    }
+}
